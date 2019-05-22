1dd47bf8119d46f1406ce290a4420cc54b536744
Meyer <ehlmeyer@gmail.com>
2019-05-22 14:13:14 -0500

    clearance level stuff works

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,7 +5,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     if(req.isAuthenticated()) {
         console.log('req.user:', req.user);
-        pool.query('SELECT * FROM "secret";')
+        let query = 'SELECT * FROM "secret" WHERE "secrecy_level"<=$1;'
+        pool.query(query,[req.user.clearance_level])
             .then(results => res.send(results.rows))
             .catch(error => {
                 console.log('Error making SELECT for secrets:', error);
